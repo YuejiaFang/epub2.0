6122c2ab58e092caaf9a5019535808868ae95d11
YuejiaFang <fangyuejia@gmail.com>
2022-01-17 20:59:47 -0500

    create a book class

---
+++ epub2.0/Model/Book.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace epub2._0.Model
+{
+    internal class Book
+    {
+        
+        public static
+    }
+}
